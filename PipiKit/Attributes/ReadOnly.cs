bd58d6cd47b318c15511afc6a22f4eeaece9f82c
陈皮皮 <952157129@qq.com>
2023-02-17 20:00:48 +0800

    update.

--- PipiKit/Attributes/ReadOnly.cs
+++ PipiKit/Attributes/ReadOnly.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 namespace PipiKit
 {
 
-    public class ReadOnlyAttribute : PropertyAttribute
-    {
-
-    }
+    public class ReadOnlyAttribute : PropertyAttribute { }
 
     [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
     public class ReadOnlyDrawer : PropertyDrawer
@@ -20,9 +17,47 @@ namespace PipiKit
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            GUI.enabled = false;
-            EditorGUI.PropertyField(position, property, label, true);
-            GUI.enabled = true;
+            string value;
+            switch (property.propertyType)
+            {
+                case SerializedPropertyType.Integer:
+                    value = property.intValue.ToString();
+                    break;
+                case SerializedPropertyType.Boolean:
+                    value = property.boolValue.ToString();
+                    break;
+                case SerializedPropertyType.Float:
+                    value = property.floatValue.ToString("0.00000");
+                    break;
+                case SerializedPropertyType.String:
+                    value = property.stringValue;
+                    break;
+                case SerializedPropertyType.Enum:
+                    value = property.enumNames[property.enumValueIndex];
+                    break;
+                default:
+                    value = null;
+                    break;
+            }
+
+            EditorGUI.BeginDisabledGroup(true);
+            {
+                if (value != null)
+                {
+                    // EditorGUI.LabelField(position, label.text, value);
+                    EditorGUILayout.BeginHorizontal();
+                    {
+                        EditorGUILayout.LabelField(label, GUILayout.Width(EditorGUIUtility.labelWidth));
+                        EditorGUILayout.SelectableLabel(value, EditorStyles.textField, GUILayout.Height(EditorGUIUtility.singleLineHeight));
+                    }
+                    EditorGUILayout.EndHorizontal();
+                }
+                else
+                {
+                    EditorGUI.PropertyField(position, property, label, true);
+                }
+            }
+            EditorGUI.EndDisabledGroup();
         }
 
     }
